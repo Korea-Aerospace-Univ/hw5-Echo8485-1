877fbebf51c56d8579354c8405043ce71ef1ef44
Echo8485 <steve1921@kau.kr>
2026-05-04 15:36:35 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -6,17 +6,17 @@ int main() {
 
     int found = 0;
 
-    for (int a = 1; a <= N / 900; a++) {
-        for (int b = 2; b <= N / 750; b += 2) { // B는 짝수
-            for (int c = 1; c <= N / 200; c++) {
+    for (int a = 0; a <= N / 900; a++) {
+        for (int b = 0; b <= N / 750; b++) {
+            if (b % 2 != 0) continue; // b는 짝수
+
+            for (int c = 0; c <= N / 200; c++) {
 
                 int total = 900 * a + 750 * b + 200 * c;
 
-                if (total == N) {
-                    if (c <= b) {
-                        printf("%d %d %d\n", a, b, c);
-                        found = 1;
-                    }
+                if (total == N && c <= b) {
+                    printf("%d %d %d\n", a, b, c);
+                    found = 1;
                 }
             }
         }
@@ -26,5 +26,5 @@ int main() {
         printf("none\n");
     }
 
-    return 0;  
+    return 0;
 }
