5c401f3df91a3e658246bda86cc258ca9d3e7777
Echo8485 <steve1921@kau.kr>
2026-05-04 15:42:40 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -6,13 +6,13 @@ int main() {
 
     int found = 0;
 
-    for (int a = 1; a <= N / 900; a++) {   // a는 1부터 시작
-        for (int b = 2; b <= N / 750; b += 2) { // b는 짝수
-            for (int c = 1; c <= N / 200; c++) {
+    for (int a = 1; a <= N / 900; a++) {          // A 최소 1
+        for (int b = 2; b <= N / 750; b += 2) {   // B 짝수 + 최소 2
+            for (int c = 1; c <= N / 200; c++) {  // C 최소 1
 
                 int total = 900 * a + 750 * b + 200 * c;
 
-                if (total == N && c <= b) {
+                if (total == N && (c < a || c < b)) {
                     printf("%d %d %d\n", a, b, c);
                     found = 1;
                 }
